abed70072a37695cf537223fef3731b724610434
Bobby Offiong <89223802+BobbyOffiong@users.noreply.github.com>
2025-07-21 12:18:39 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <div>
-    <footer className="w-full mt-auto py-4 text-center text-sm text-gray-500 bg-gray-100">
+    <footer className="w-full mt-auto py-4 text-center text-sm text-gray-600 bg-gray-200">
       &copy; {new Date().getFullYear()} B's Spelling Bee Organizer. All rights reserved.
     </footer></div>
   );
